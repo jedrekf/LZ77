36c5ab086dc39267039ec356a9b7a463d6ab4353
jedrek <jedrek@fijalkowscy.com>
2018-05-08 21:07:33 +0200

    Added global variables with window size parameters

--- structs.h
+++ structs.h
@@ -5,3 +5,8 @@ struct Triple {
     int     C; // length of substring
     char    S; // symbol
 };
+
+// I guess dictionary and input buffor sizes should be equal so both are half of window size.
+int WINDOW_SIZE = 4096;
+int DICTIONARY_SIZE = 2048;
+int INPUT_BUFFOR_SIZE = 2048;
