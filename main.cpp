e7e24eee8c902b302b033b88434ef0ed89f17666
jedrek <jedrek@fijalkowscy.com>
2018-06-01 18:41:22 +0200

    Added saving algorithm times for testing.

--- main.cpp
+++ main.cpp
@@ -8,26 +8,35 @@ Window WINDOW_PARAMS = {4096, 2048, 2048};
 int main(int argc, char* argv[]) {
 	auto args = get_args(argc, argv);
 	auto output_file_path = std::get<1>(args);
+	
+	int elapsed_time;
 
 	bool compress = std::get<2>(args);
-	if(compress) {
-		auto data = load_from_file(std::get<0>(args));
+	try{
+		if(compress) {
+			auto data = load_from_file(std::get<0>(args));
 
-		std::cout << "Encoding string : "<< data <<std::endl;
-		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-		auto result = get_compressed_data(data);
-		std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-		std::cout << "Compression took nanoseconds = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
-		
-		save_encoded_to_file(result, output_file_path);
-	} else {
-		auto data = load_encoded_from_file(std::get<0>(args));
-		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-		auto decompressed_str = decompress(data);
-		std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-		std::cout << "Decompression took nanoseconds = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
-		std::cout << "Decoded string  : " << decompressed_str << std::endl;
-		save_to_file(decompressed_str, output_file_path);
+			std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+			auto result = get_compressed_data(data);
+			std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+			elapsed_time = (int)std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count();
+			std::cout << elapsed_time <<std::endl;
+
+			save_encoded_to_file(result, output_file_path);
+		} else {
+			auto data = load_encoded_from_file(std::get<0>(args));
+
+			std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+			auto decompressed_str = decompress(data);
+			std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+			elapsed_time = (int)std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count();
+			std::cout<< elapsed_time <<std::endl;
+
+			save_to_file(decompressed_str, output_file_path);
+		}
+	}catch(const std::exception& e){
+		std::cout << e.what() << std::endl;
+		exit(-1);
 	}
 
 	return 0;	
@@ -55,7 +64,7 @@ std::tuple<std::string, std::string, bool> get_args(int argc, char* argv[]) {
 		print_usage(argv);
 		exit(-1);
 	}
-
+	
 	auto file_path = argv[1];
 	auto output_file_path = argv[2];
 	return std::make_tuple(file_path, output_file_path, compress);
--- test.sh
+++ test.sh
@@ -10,25 +10,38 @@ echo ; echo ; echo ;
 #print variable on a screen
 echo "Launching tests for LZ77 algorithm..."
 
+declare -a results=()
+all_tests_passed=true
 
 for fn in `ls -d -1 ./tests/input_files/*.*`; do
-    echo "the next file is $fn"
+    echo
+    echo "running test: "$fn
     file_len=$(wc -c <"$fn")
     echo "File length: "$file_len
     
     temp_enc=fn"_enc"
     temp_dec=fn"_dec"
     
-    echo "Running compression..."
-    ./main $fn $temp_enc "compress"
-    if [ $? -ne 0 ]
+    compression_time=`./main $fn $temp_enc "compress"`
+    compression_result=$?
+    if [ $compression_result -ne 0 ]
     then
-        echo "${RED}ERROR:{$NC} file not successfuly encoded: $fn"
+        echo "${RED}ERROR:${NC} file not successfuly encoded: $fn"
+        all_tests_passed=false
+    else
+        echo "compression time: "$compression_time
+        results+=($compression_time)
     fi
-    ./main $temp_enc $temp_dec "decompress"
-    if [ $? -ne 0 ]
+
+    decompression_time=`./main $temp_enc $temp_dec "decompress"`
+    decompression_result=$?
+    if [ $decompression_result -ne 0 ]
     then
-        echo -e "${RED}ERROR:{$NC} file not successfuly decoded: $fn"
+        echo -e "${RED}ERROR:${NC} file not successfuly decoded: $fn"
+        all_tests_passed=false
+    else
+        echo "Decompression time: "$decompression_time
+        results+=($decompression_time)
     fi
 
     #compare initial file with the compressed -> decompressed one
@@ -36,8 +49,29 @@ for fn in `ls -d -1 ./tests/input_files/*.*`; do
     if [ $? -ne 0 ]
     then
         echo -e "${RED}ERROR:${NC} Decoded file is not the same as the input file $fn. Something went wrong. :("
+        all_tests_passed=false
     fi
 
     `rm $temp_enc $temp_dec`
 done
 
+echo
+
+if $all_tests_passed;then
+    #save results to csv file
+    echo "compression, decompression" > test_results.csv
+    i=0
+    for result in "${results[@]}"; do
+        if [ $(($i%2)) -eq 0 ]; then
+            echo -n "$result," >> test_results.csv
+        else
+            echo "$result," >> test_results.csv
+        fi
+        i=$((i+1))
+    done
+    echo "Tests finished successfuly, time elapsed results saved in test_results.csv"
+else
+    echo "Not All tests finished successfuly."
+fi
+
+ 
